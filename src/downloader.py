99c64514a8c48f4ff2641e5e4e5c68f30bf655bf
rpbot_mac <71392285+rp-bot@users.noreply.github.com>
2023-04-28 00:12:48 -0400

    pulling genres

--- src/downloader.py
+++ src/downloader.py
@@ -6,26 +6,30 @@ from pprint import pprint
 DRIVER = install_driver()
 
 
-def get_song_details():
+def get_genres():
+    genres = []
     genre_tab = DRIVER.find_element(By.XPATH, '//*[@id="mainContent"]/div[7]')
     genre_list = genre_tab.find_elements(By.TAG_NAME, "a")
 
     time.sleep(1)
 
-    for genre in tqdm(genre_list, desc=f"scraping midi files from genres: "):
+    for genre in genre_list:
         link = genre.get_attribute("href")
-        DRIVER.execute_script(f"window.open('{link}', '_blank');")
-        # switch to the second tab
-        DRIVER.switch_to.window(DRIVER.window_handles[1])
-        # //*[@id="mainContent"]/div[2]/div/a[2]
-        artists = DRIVER.find_elements(By.CLASS_NAME, "genre-band-container")
-        for artist in range(2, len(artists)+2):
-            pprint(artists[artist-2].text)
-        time.sleep(60)
+        genres.append({
+            "genre": genre.text,
+            "url": link
+        })
 
-    DRIVER.implicitly_wait(30)
+    return genres
 
+# DRIVER.execute_script(f"window.open('{link}', '_blank');")
+# DRIVER.switch_to.window(DRIVER.window_handles[1])
+
+
+# artists = DRIVER.find_elements(By.CLASS_NAME, "genre-band-container")
+ # pprint(artists[artist-2].text)
 
 if __name__ == '__main__':
     DRIVER.get("https://freemidi.org/all")
-    get_song_details()
+    x = get_genres()
+    pprint(x)
