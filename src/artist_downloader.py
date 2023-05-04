f386122549602572461f0839ee22c34dc49c5558
rpbot_mac <71392285+rp-bot@users.noreply.github.com>
2023-05-04 00:15:49 -0400

    downloaded artists

--- src/artist_downloader.py
+++ src/artist_downloader.py
@@ -6,37 +6,53 @@ import sqlite3
 DRIVER = install_driver()
 
 conn = sqlite3.connect('data/db/UltimateMidi.sqlite3')
+c = conn.cursor()
+
+STR_TABLE = str.maketrans({"-": "_", " ": "_"})
 
 
 def retrieve_from_genres():
-    c = conn.cursor()
     select_data = '''SELECT * FROM genres'''
     c.execute(select_data)
     rows = c.fetchall()
-    c.close()
-    conn.close()
     return rows
 
 
-def add_artists_bands(genre):
-    c = conn.cursor()
+def add_artists_bands(genre_list):
+    # c = conn.cursor()
 
-    create_table = '''CREATE TABLE IF NOT EXISTS artists_bands (ID INTEGER PRIMARY KEY,genre TEXT,url TEXT)'''
-    c.execute(create_table)
-    conn.commit()
+    # create_table = '''CREATE TABLE IF NOT EXISTS artists_bands (ID INTEGER PRIMARY KEY,genre TEXT,url TEXT)'''
+    # c.execute(create_table)
+    # conn.commit()
 
     DRIVER.switch_to.window(DRIVER.window_handles[-1])
     artists = DRIVER.find_elements(By.CLASS_NAME, "genre-band-container")
-    for artist in range(len(artists)+2):
-        curr = artists[artist -
-                       2].find_elements(By.TAG_NAME, "a")[2].get_attribute("href")
-    time.sleep(60)
+    for i, artist in enumerate(artists):
+        artist_name = artist.text
+        artist_url = artist.find_elements(
+            By.TAG_NAME, "a")[2].get_attribute("href")
+        insert_data = f'''INSERT OR IGNORE INTO {genre_list[1].translate(STR_TABLE)} (artist_ID, artist_name, artist_url) VALUES (?, ?, ?)'''
+
+        c.execute(insert_data, [i, artist_name, artist_url])
+        conn.commit()
+        print(artist_name)
+    # time.sleep(60)
+
+
+def create_genre_table(genre_list):
+
+    create_table = f'''CREATE TABLE IF NOT EXISTS {genre_list[1].translate(STR_TABLE)} (artist_ID INTEGER PRIMARY KEY,artist_name TEXT,artist_url TEXT)'''
+    c.execute(create_table)
 
 
 if __name__ == '__main__':
     genres = retrieve_from_genres()
     for genre in genres:
-        DRIVER.execute_script(f"window.open('{genre[-1]}');")
+        # create_genre_table(genre)
+        print(genre)
+        DRIVER.execute_script(f"window.open('{genre[2]}');")
         add_artists_bands(genre)
 
         # pprint(genre)
+    c.close()
+    conn.close()
