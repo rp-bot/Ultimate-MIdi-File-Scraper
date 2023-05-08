1a45da87bed62fc1234ad3b8afb53508c2e126fa
rpbot_mac <71392285+rp-bot@users.noreply.github.com>
2023-05-08 12:09:31 -0400

    midi download working

--- src/MIDI_downloader.py
+++ src/MIDI_downloader.py
@@ -23,35 +23,51 @@ def get_all_urls(genres):
             urls_dict.update({genre[1].translate(STR_TABLE): urls_list})
         except sqlite3.OperationalError:
             pprint(
-                f'No table named: {genre[1].translate(STR_TABLE)}_songs, but the loop continued on')
+                f'No table named: {genre[1].translate(STR_TABLE)}_songs, \nbut the loop continued on')
             continue
 
     return urls_dict
 
 
-def download_MIDI(urls_dict: dict, genre: str):
+def add_dl_urls(data: list):
+    push_command = f'''INSERT OR IGNORE INTO dl_urls (artist_ID, artist_name, song_name,song_url, download_url) VALUES (?, ?, ?, ?, ?)'''
+
+    midi_db_cursor.execute(push_command, data)
+    midi_db_conn.commit()
+    print("done")
+
+
+def download_MIDI(urls_dict: dict):
     # for all urls in the dictionary in the given
-    pass
+    for genre, artists in urls_dict.items():
+        for artist in artists:
+            DRIVER.get(artist[3])
+            download_link = DRIVER.find_element(
+                By.ID, "downloadmidi").get_attribute("href")
+
+            plus_dl_link = artist+(download_link,)
+            add_dl_urls(plus_dl_link)
+            time.sleep(10)
 
 
 if __name__ == '__main__':
 
-    url_db = sqlite3.connect('data/db/UltimateMidi.sqlite3')
-    url_db_cursor = url_db.cursor()
+    url_db_conn = sqlite3.connect('data/db/UltimateMidi.sqlite3')
+    url_db_cursor = url_db_conn.cursor()
 
-    midi_db = sqlite3.connect('data/db/AllMIDI.sqlite3')
-    midi_db_cursor = midi_db.cursor()
+    midi_db_conn = sqlite3.connect('data/db/AllMIDI.sqlite3')
+    midi_db_cursor = midi_db_conn.cursor()
 
-    # DRIVER = install_driver()
+    DRIVER = install_driver()
 
     genres = get_genres()
 
     all_urls_dict = get_all_urls(genres)
 
-    download_MIDI(all_urls_dict, genres[0][1])
+    download_MIDI(all_urls_dict)
 
     url_db_cursor.close()
-    url_db.close()
+    url_db_conn.close()
 
     midi_db_cursor.close()
-    midi_db.close()
+    midi_db_conn.close()
